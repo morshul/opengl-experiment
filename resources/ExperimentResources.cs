2288183c8c00d63a99ce59b0a1317ee78d609b0c
morshul <morshu0@proton.me>
2023-01-06 23:21:34 -0300

    feat: initial commit

---
+++ resources/ExperimentResources.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace experiment.Game.Resources;
+
+public static class ExperimentResources
+{
+    public static Assembly Assembly => typeof(ExperimentResources).Assembly;
+}
